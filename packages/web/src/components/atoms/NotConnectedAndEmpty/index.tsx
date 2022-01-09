85905a2eaf18996b81f142f7538074ef296d37e5
Hideo Hattori <hhatto.jp@gmail.com>
2022-01-09 15:33:02 +0900

    fix build error

--- packages/web/src/components/atoms/NotConnectedAndEmpty/index.tsx
+++ packages/web/src/components/atoms/NotConnectedAndEmpty/index.tsx
@@ -1,26 +1,12 @@
 import { Empty } from 'antd'
 import { EmptyProps } from 'antd/lib/empty'
 import React from 'react'
-import { useConnectWallet, useProvider } from 'src/fixtures/wallet/hooks'
-import { ButtonWithGradient } from '../ButtonWithGradient'
-
-const SignIn = () => {
-  const { connect, isConnecting } = useConnectWallet()
-  return (
-    <ButtonWithGradient loading={isConnecting} onClick={connect}>
-      Sign in
-    </ButtonWithGradient>
-  )
-}
+import { useProvider } from 'src/fixtures/wallet/hooks'
 
 export const NotConnectedAndEmpty = (props: EmptyProps) => {
   const { accountAddress } = useProvider()
   const isConnected = Boolean(accountAddress)
   const description = isConnected ? 'No Data' : 'Please sign in'
 
-  return (
-    <Empty description={description} {...props}>
-      {isConnected ? undefined : <SignIn />}
-    </Empty>
-  )
+  return <Empty description={description} {...props} />
 }
