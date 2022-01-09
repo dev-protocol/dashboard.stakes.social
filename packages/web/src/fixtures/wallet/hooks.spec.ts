ae6d0a2f319476ace2a501718709b27bae2c40bd
Hideo Hattori <hhatto.jp@gmail.com>
2022-01-09 16:09:46 +0900

    remove unnecessary code

--- packages/web/src/fixtures/wallet/hooks.spec.ts
+++ packages/web/src/fixtures/wallet/hooks.spec.ts
@@ -1,26 +1,8 @@
-import { renderHook, act } from '@testing-library/react-hooks'
-import { useConnectWallet, useDetectChain } from './hooks'
-import { connectWallet, detectChain } from 'src/fixtures/wallet/utility'
+import { renderHook } from '@testing-library/react-hooks'
+import { useDetectChain } from './hooks'
+import { detectChain } from 'src/fixtures/wallet/utility'
 
 jest.mock('src/fixtures/wallet/utility.ts')
-
-describe('useConnectWallet', () => {
-  test('connect & disconnect', async () => {
-    ;(connectWallet as jest.Mock).mockImplementation(() => Promise.resolve(true))
-    const { result } = renderHook(() => useConnectWallet())
-    await act(async () => {
-      await result.current.connect()
-    })
-    expect(result.current.isConnected).toBe(true)
-    expect(result.current.isConnecting).toBe(false)
-
-    await act(async () => {
-      await result.current.disconnect()
-    })
-    expect(result.current.isConnected).toBe(false)
-    expect(result.current.isConnecting).toBe(false)
-  })
-})
 
 // TODO: Test it!
 describe.skip('useDetectChain', () => {
